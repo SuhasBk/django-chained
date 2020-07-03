54806751717b875b2ec71fd6c3018b0b78084e4c
Suhas BK <kowligi1998@gmail.com>
2020-07-03 17:07:10 +0530

    first app completed

--- pwd_generator_project/pwd_generator/urls.py
+++ pwd_generator_project/pwd_generator/urls.py
@@ -18,5 +18,6 @@ from generator import views
 
 urlpatterns = [
     path('',views.home),
-    path('password/', views.password,name='password')
+    path('password/', views.password,name='password'),
+    path('about/',views.about,name='about')
 ]
