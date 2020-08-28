62620942cdc945274d31e1309cb3fd161e45a05d
Suhas Kowligi <kowligi1998@gmail.com>
2020-08-28 19:57:35 +0530

    hide secret key

--- dh_epaper_project/dh_epaper/settings.py
+++ dh_epaper_project/dh_epaper/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fxc#thqf66s#$ua&f9)*-5tk_ti_4rh+z(c1#h0$x(x##4g!tq'
+SECRET_KEY = os.environ.get('DH_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
