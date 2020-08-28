1bb4547bf716f54374a77f2f531bce7ad42d4ef3
Suhas Kowligi <kowligi1998@gmail.com>
2020-08-28 21:44:51 +0530

    Update settings.py

--- todowoo_project/todowoo/settings.py
+++ todowoo_project/todowoo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'us=lt1vp#&1aon-w%0)(xd1hbxalrnd^0d82!(2q!11#fn-dws'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
