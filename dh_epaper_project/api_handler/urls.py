1a1bf5f8718a36a1daff620f3a35990545185678
Suhas Kowligi <kowligi1998@gmail.com>
2020-08-31 15:20:25 +0530

    separated dh-epaper project

--- dh_epaper_project/api_handler/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import DeccanApi, FileExists
-
-urlpatterns = [
-    path('', DeccanApi.as_view()),
-    path('find/', FileExists.as_view())
-]
--- dh_epaper_project/html_ui/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PdfCreatorConfig(AppConfig):
-    name = 'pdf_creator'
